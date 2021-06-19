16b4d57ad37867c00da9c753c560cc6300f8cdd4
Nikita <Nkanunov@gmail.com>
2021-06-19 13:37:27 +0200

    Fixed main screen if there is a access_token

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:webant_test_app/blocs/load_image_bloc/load_image_bloc.dart';
 import 'package:webant_test_app/blocs/load_popular_images_bloc/load_popular_images_bloc.dart';
 import 'package:webant_test_app/blocs/profile_bloc/profile_bloc.dart';
 import 'package:webant_test_app/resources/image_api/image_repository.dart';
+import 'package:webant_test_app/resources/shared_prefs.dart';
 import 'package:webant_test_app/resources/user_api/user_repository.dart';
 import 'package:webant_test_app/screens/main_screen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:webant_test_app/screens/splash_screen.dart';
 import 'package:webant_test_app/screens/welcome_screen.dart';
 
 void main() {
@@ -17,6 +16,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  Future<String?> getAccessToken() async {
+    SharedPrefs _prefs = SharedPrefs();
+    String? _accessToken = await _prefs.read('access_token');
+    return _accessToken;
+  }
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
@@ -36,12 +41,21 @@ class MyApp extends StatelessWidget {
           )
         ],
         child: MaterialApp(
-            localizationsDelegates: AppLocalizations.localizationsDelegates,
-            supportedLocales: AppLocalizations.supportedLocales,
-            theme: ThemeData(
-              primarySwatch: Colors.blue,
-            ),
-            home: WelcomeScreen()),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: FutureBuilder<String?>(
+              future: getAccessToken(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return MainScreen();
+                } else {
+                  return WelcomeScreen();
+                }
+              }),
+        ),
       ),
     );
   }
