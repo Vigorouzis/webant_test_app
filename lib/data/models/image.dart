8867a3273cab57ab29a610537dfee3e386dd66f3
Nikita <Nkanunov@gmail.com>
2021-07-19 11:32:58 +0200

    Fix some error

--- lib/data/models/image.dart
+++ lib/data/models/image.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
-import 'package:intl/intl.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:webant_test_app/domain/entities/image_entity.dart';
 
